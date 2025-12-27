473d7b0d01da3f019668079857af4507275c81d5
Stratos Thivaios <sthivaios@kioydiolabs.org>
2025-12-28 01:19:26 +0200

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# sthivaios.dev
+
+This is my personal website!
+
+The repository contains multiple applications including the Sanity Studio and the NextJS frontend.
+
+You should check out _sanity.io_ to learn more about what the Studio is if this is confusing.
+
+Other than that, it uses shadcn/ui (duh, lol) and Tailwind, and uhm... probably other stuff that I'm forgetting.
+
+
+### Contributions
+
+I obviously don't expect (or want) random PRs here lol, but feedback
+is more than welcome!
+
+If you
+were on my website and you thought something in it kinda sucks, open an issue here, and I'll look into it.
---
+++ apps/studio/schemaTypes/postType.ts
@@ -0,0 +1,82 @@
+export default {
+  name: 'post',
+  title: 'Blog Post',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+      validation: (rule) => rule.required().error("Posts are required to have a slug")
+    },
+    {
+      name: 'mainImage',
+      title: 'Main Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          styles: [
+            {title: 'Normal', value: 'normal'},
+            {title: 'H1', value: 'h1'},
+            {title: 'H2', value: 'h2'},
+            {title: 'H3', value: 'h3'},
+            {title: 'Quote', value: 'blockquote'},
+            {title: 'Code', value: 'code'},
+          ],
+          lists: [
+            {title: 'Bullet', value: 'bullet'},
+            {title: 'Numbered', value: 'number'},
+          ],
+          marks: {
+            decorators: [
+              {title: 'Strong', value: 'strong'},
+              {title: 'Emphasis', value: 'em'},
+              {title: 'Code', value: 'code'},
+            ],
+            annotations: [
+              {
+                name: 'link',
+                type: 'object',
+                title: 'Link',
+                fields: [
+                  {
+                    name: 'href',
+                    type: 'url',
+                    title: 'URL',
+                  },
+                ],
+              },
+            ],
+          },
+        },
+        {
+          type: 'image',
+          options: {hotspot: true},
+        },
+      ],
+    },
+  ],
+}
