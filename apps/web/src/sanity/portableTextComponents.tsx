dd6f57ccff587fab4c884075bc1dcc1f4476bf76
Stratos Thivaios <sthivaios@kioydiolabs.org>
2025-12-28 05:25:02 +0200

    custom component for inline images

---
+++ apps/web/src/sanity/portableTextComponents.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import { PortableTextComponents } from "next-sanity";
+import { sanityImageUrl } from "@/sanity/image";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+
+export const components: PortableTextComponents = {
+  types: {
+    inlineImage: (props) =>
+      props.value ? (
+        <div className="flex flex-row items-center justify-center">
+          <div className="flex flex-col items-center gap-0 w-full max-w-2xl mt-6">
+            <AspectRatio ratio={16 / 9} className="rounded-lg w-full max-w-2xl">
+              <Image
+                src={sanityImageUrl(props.value.image)
+                  .quality(80)
+                  .auto("format")
+                  .url()}
+                alt={props?.value?.alt || ""}
+                className="object-contain not-prose"
+                fill
+              />
+            </AspectRatio>
+            <p className="text-center px-5 text-muted-foreground">
+              {props?.value?.caption}
+            </p>
+          </div>
+        </div>
+      ) : null,
+  },
+};
