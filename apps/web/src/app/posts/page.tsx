702057aeae6a771d2344957d0ccdbab39c95129a
Stratos Thivaios <sthivaios@kioydiolabs.org>
2025-12-28 03:20:34 +0200

    more theming and ui work

--- apps/web/src/app/posts/page.tsx
+++ apps/web/src/app/posts/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { defineQuery } from "next-sanity";
 import { sanityFetch } from "@/sanity/live";
+import { Card } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
 
 const POSTS_QUERY = defineQuery(`*[
   _type == "post"
@@ -11,29 +13,30 @@ export default async function IndexPage() {
   const { data: posts } = await sanityFetch({ query: POSTS_QUERY });
 
   return (
-    <main className="flex min-h-screen flex-col p-24 gap-12">
-      <h1 className="text-4xl font-bold tracking-tighter text-gray-900 dark:text-white">
-        Posts
-      </h1>
-      <ul className="grid grid-cols-1 gap-12 lg:grid-cols-2">
+    <main className="flex flex-col w-full items-center justify-center gap-10">
+      <h1 className="text-4xl font-bold">Posts</h1>
+      <ul className="flex flex-col w-[40%] items-center justify-center gap-10">
         {posts.map((post) => (
-          <li
-            className="bg-white dark:bg-gray-800 p-4 rounded-lg shadow-sm dark:shadow-gray-900/20"
-            key={post._id}
-          >
-            <Link
-              className="hover:underline block"
-              href={`/posts/${post?.slug?.current}`}
-            >
-              <h2 className="text-xl font-semibold text-gray-900 dark:text-white">
-                {post?.title}
-              </h2>
-              {post?.publishedAt && (
-                <p className="text-gray-500 dark:text-gray-400">
-                  {new Date(post.publishedAt).toLocaleDateString()}
-                </p>
-              )}
-            </Link>
+          <li className="w-full" key={post._id}>
+            <Card className="p-5 w-full">
+              <Link
+                className="hover:underline block"
+                href={`/posts/${post?.slug?.current}`}
+              >
+                <div className="flex flex-col gap-2">
+                  <h2 className="text-xl font-semibold text-gray-900 dark:text-white">
+                    {post?.title}
+                  </h2>
+                  {post?.publishedAt && (
+                    <div className="flex flex-row w-full">
+                      <Badge className="">
+                        {new Date(post.publishedAt).toLocaleDateString()}
+                      </Badge>
+                    </div>
+                  )}
+                </div>
+              </Link>
+            </Card>
           </li>
         ))}
       </ul>
