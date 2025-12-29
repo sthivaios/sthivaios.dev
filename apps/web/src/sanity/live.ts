911c99106a4d02bff6d8b3c73ec3d729588723df
Stratos Thivaios <sthivaios@kioydiolabs.org>
2025-12-29 08:02:04 +0200

    use tokens in defineLive

--- apps/web/src/sanity/live.ts
+++ apps/web/src/sanity/live.ts
@@ -2,5 +2,7 @@ import { defineLive } from "next-sanity/live";
 import { client } from "@/sanity/client";
 
 export const { sanityFetch, SanityLive } = defineLive({
-  client
+  client,
+  browserToken: process.env.SANITY_BROWSER_TOKEN,
+  serverToken: process.env.SANITY_SERVER_TOKEN,
 });
